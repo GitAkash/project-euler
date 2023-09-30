66433982d90fbbe95fefdc46a086842d5403346f
GitAkash <akashmondalnl@gmail.com>
2023-09-30 14:24:01 +0200

    Hey :D

---
+++ Julia/Problem 1.jl
@@ -0,0 +1,16 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+
+sumation = 0
+for i in 0:999
+    if i % 3 == 0 || i % 5 == 0
+        sumation += i
+    end
+end
+
+println(sumation)
